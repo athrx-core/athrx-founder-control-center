2ce6e4372baf91c5f65d48b78346aa1346f206b0
ATHRX Founder <YOUR_GITHUB_EMAILoperator@athrx.ai>
2026-04-11 11:31:36 +0400

    fix tailwind content scan

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,8 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {}
